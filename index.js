8434ae54ac469bc9e169b7b404d4f63896c62e71
yangchenguang <yangchenguang@fpwis.com>
2024-02-18 17:09:38 +0800

    feat: 添加验证

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+/*
+ * @Author: yangchenguang
+ * @Description:
+ * @Date: 2023-09-28 16:52:20
+ * @LastEditors: yangchenguang
+ * @LastEditTime: 2024-02-18 17:08:58
+ */
+
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const { body, validationResult } = require("express-validator");
 
 // 服务器及端口
 const app = express();
@@ -10,9 +18,8 @@ const port = 3001;
 // 允许所有跨域请求
 app.use(cors());
 // 使用 body-parser 中间件解析请求体数据
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.raw({ type: "application/octet-stream", limit: "10mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // 自定义中间件
 app.use((req, res, next) => {
   console.log("请求方法:", req.method);
@@ -27,15 +34,42 @@ app.use((req, res, next) => {
 
 // 处理根路径的 GET 请求
 app.get("/", (req, res) => {
-  res.send(`我是李坤夏今年${Math.floor(Math.random() * 100)}岁`);
+  res.send({
+    success: true,
+    data: `我是李坤夏今年${Math.floor(Math.random() * 100)}岁`,
+  });
 });
 
 // 处理根路径的 post 请求
-app.post("/getUser", (req, res) => {
-  res.send({
-    name: `我是李坤夏今年${Math.floor(Math.random() * 100)}岁`,
-  });
-});
+app.post(
+  "/getUser",
+  [
+    body("username").not().isEmpty().withMessage("用户名不能为空"),
+    body("password").isLength({ min: 6 }).withMessage("密码长度不能少于6位"),
+  ],
+  (req, res) => {
+    console.log(req.body, "req.body");
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({
+      //   success: false,
+      //   errors: errors.array()[0].msg,
+      // });
+      res.send({
+        success: false,
+        errorMessage: errors.array()[0].msg,
+      });
+      return;
+    }
+    setTimeout(() => {
+      res.send({
+        success: true,
+        ...req.body,
+      });
+    }, 1000);
+  }
+);
 
 // 启动服务器
 app.listen(port, () => {
